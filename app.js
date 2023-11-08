74ce400748a3e7028d2ce878700de83af70b5e06
violet <blueangle1004@gmail.com>
2023-11-08 20:59:41 +0900

    1108

---
+++ app.js
@@ -0,0 +1,16 @@
+// const와 let, var의 차이
+// console.log() == printf
+// undefined 와 null의 차이
+// array에 데이터 타입이 다를 수 있음, []사용
+// array. 을 하면 여러기능들이 기본적으로 존재,
+// 존재하지 않는 인덱스에 접근하면 어떻게 될까? -> undefined 출력
+// const name={
+// name: sdfghjk;
+//
+// }
+// 이런식으러 구조체 선언도 가능, . 으로 접근 혹은 name["name"] 이런식
+// 근데 구조체가 const라고 안에 있는 요소들도  const인 것은 아니다. 그 구조가 const인것,
+// 근데 name.n
+// 함수선언은 function name(){}로 함, 매개변수를 받을 떄 형을 안정해주고 이름으로만 설정
+// 구조체 안에 함수를 선언할 때에는 name: function(){} 형식
+alert("hi");
